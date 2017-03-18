44e1bf8136551546e99650d633bb6574d11a19f9
azuraiii <c14429822@mydit.ie>
2017-03-18 14:41:23 +0000

    initial server

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //pass in hostname and port of the server
+	    Server server = new Server("localhost",222);
+    }
+}
