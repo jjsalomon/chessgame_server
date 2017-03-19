1e8761ec295168ac28f7fd9674793f5822210551
azuraiii <c14429822@mydit.ie>
2017-03-19 16:15:12 +0000

    working with client, took out network abstract class

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,18 +1,26 @@
 package com.company;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by azkei on 16/03/2017.
  */
-public class Server extends Network implements Runnable{
+public class Server extends Thread implements Runnable{
 
+    private ObjectOutputStream dos;
+    private ObjectInputStream dis;
     private String hostName;
     private Thread thread;
     private ServerSocket serverSocket;
     private final int listenPort;
+    private Socket socket;
+
+    private boolean connection = false;
 
     public Server(final String host,final int listen_port) {
         super("SERVER");
@@ -34,18 +42,64 @@ public class Server extends Network implements Runnable{
 
     public void run() {
         System.out.println("SERVER: Thread communication working");
-        try{
-            listenForConnection();
-            getStreams();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+
+        //keep listening for connections
+          while(true){
+              listenForConnection();
+          }
+
+
+
+
     }
 
     private void listenForConnection(){
         try {
             socket = serverSocket.accept();
             System.out.println("SERVER: Connection received from: " + socket.getInetAddress().getHostName());
+            dos = new ObjectOutputStream(socket.getOutputStream());
+            dos.flush();
+            dis = new ObjectInputStream(socket.getInputStream());
+            //message client saying thanks
+            sendData("Thanks for connecting");
+            connection = true;
+            if(connection == true){
+                try {
+                    Object message = dis.readObject();
+                    System.out.println(message);
+                    connection = false;
+                }catch(IOException e){
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnection(){
+        try{
+            if(dos != null){
+                dos.close();
+            }
+            if(dis != null){
+                dis.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void sendData(final Object obj_send){
+        try{
+            dos.writeObject(obj_send);
+            dos.flush();
         }catch(IOException e){
             e.printStackTrace();
         }
