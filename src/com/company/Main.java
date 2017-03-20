4321c0f28e95e07d3e4da0254b4ba34de89fc846
azuraiii <c14429822@mydit.ie>
2017-03-20 00:46:05 +0000

    server code working fine

--- src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        //pass in hostname and port of the server
-	    Server server = new Server("localhost",222);
-    }
-}
